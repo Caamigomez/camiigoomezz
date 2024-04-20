cf297382aeafbad6a9dda3e619b90cd99f3d0f49
Caamigomez <camilagomezkouyoumdjian@gmail.com>
2024-04-20 12:03:22 -0300

    a

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ document.getElementById("mostrarServicios").addEventListener("click", function()
     document.getElementById("servicios").classList.toggle("mostrar");
 });
 
+document.getElementById("mostrarServicios").addEventListener("click", function() {
+    document.getElementById("banner3").classList.toggle("ocultar");
+});
